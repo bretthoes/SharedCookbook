c7af87e99ab90d27f9c85c3fb832ce69ebb59bcc
Brett Hoes <bretthoes@gmail.com>
2024-09-22 21:33:02 -0400

    add event, complete initial flow to create recipe from dto

---
+++ src/Domain/Events/RecipeCreatedEvent.cs
@@ -0,0 +1,11 @@
+﻿namespace SharedCookbook.Domain.Events;
+
+public class RecipeCreatedEvent : BaseEvent
+{
+    public RecipeCreatedEvent(Recipe recipe)
+    {
+        Recipe = recipe;
+    }
+
+    public Recipe Recipe { get; }
+}
