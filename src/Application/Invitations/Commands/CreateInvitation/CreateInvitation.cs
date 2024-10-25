cf38a2875a5488854f5d34e34d345556e3ccfc15
Brett Hoes <bretthoes@gmail.com>
2024-10-25 18:54:52 -0400

    add create invite

---
+++ src/Application/Invitations/Commands/CreateInvitation/CreateInvitation.cs
@@ -0,0 +1,82 @@
+﻿using System.Threading;
+using SharedCookbook.Application.Common.Exceptions;
+using SharedCookbook.Application.Common.Interfaces;
+using SharedCookbook.Application.Users.Queries.GetUser;
+using SharedCookbook.Domain.Entities;
+using SharedCookbook.Domain.Enums;
+using SharedCookbook.Domain.Events;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace SharedCookbook.Application.Invitations.Commands.CreateInvitation;
+
+public record CreateInvitationCommand : IRequest<int>
+{
+    public required int CookbookId { get; set; }
+
+    public required string Email { get; set; }
+}
+
+public class CreateInvitationCommandHandler : IRequestHandler<CreateInvitationCommand, int>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IIdentityService _identityService;
+    private readonly IUser _user;
+
+    public CreateInvitationCommandHandler(IApplicationDbContext context, IIdentityService identityService, IUser user)
+    {
+        _context = context;
+        _identityService = identityService;
+        _user = user;
+    }
+
+    public async Task<int> Handle(CreateInvitationCommand command, CancellationToken cancellationToken)
+    {
+        var recipientId = await GetRecipientId(command.Email);
+
+        await ValidateInvitation(command.CookbookId, recipientId, cancellationToken);
+
+        return await SaveInvitation(command.CookbookId, recipientId, cancellationToken);
+    }
+
+    private async Task<int> GetRecipientId(string email)
+    {
+        var recipient = await _identityService.FindByEmailAsync(email);
+
+        return recipient?.Id ?? throw new NotFoundException(email, nameof(UserDto));
+    }
+
+    private async Task ValidateInvitation(int cookbookId, int recipientId, CancellationToken token)
+    {
+        var recipientIsAlreadyMember = await _context.CookbookMembers
+            .AnyAsync(member => member.PersonId == recipientId
+                && member.CookbookId == cookbookId,
+                token);
+        if (recipientIsAlreadyMember) throw new ConflictException("Recipient is already a member of this cookbook.");
+
+        var recipientHasPendingInvite = await _context.CookbookInvitations
+            .AnyAsync(invitation => invitation.RecipientPersonId == recipientId
+                && invitation.CookbookId == cookbookId
+                && invitation.InvitationStatus == CookbookInvitationStatus.Sent,
+                token);
+        if (recipientHasPendingInvite) throw new ConflictException("Recipient has already been invited.");
+    }
+
+    private async Task<int> SaveInvitation(int cookbookId, int recipientId, CancellationToken token)
+    {
+        var entity = new CookbookInvitation
+        {
+            CookbookId = cookbookId,
+            SenderPersonId = _user.Id,
+            RecipientPersonId = recipientId,
+            InvitationStatus = CookbookInvitationStatus.Sent,
+            ResponseDate = null,
+        };
+
+        _context.CookbookInvitations.Add(entity);
+        entity.AddDomainEvent(new InvitationCreatedEvent(entity));
+
+        await _context.SaveChangesAsync(token);
+
+        return entity.Id;
+    }
+}
---
+++ src/Application/Invitations/Commands/CreateInvitation/CreateInvitationCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿namespace SharedCookbook.Application.Invitations.Commands.CreateInvitation;
+
+public class CreateInvitationCommandValidator : AbstractValidator<CreateInvitationCommand>
+{
+    public CreateInvitationCommandValidator()
+    {
+        RuleFor(v => v.Email)
+            .MinimumLength(6)
+            .MaximumLength(256)
+            .EmailAddress()
+            .NotEmpty()
+            .WithMessage("Email must be a valid email address.");
+
+        RuleFor(v => v.CookbookId)
+            .GreaterThan(0)
+            .WithMessage("CookbookId must be greater than zero.");
+    }
+}
