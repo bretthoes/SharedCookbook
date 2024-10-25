f410a95cf05df011abce741f531cae4cac7b18e2
Brett Hoes <bretthoes@gmail.com>
2024-10-25 17:08:38 -0400

    add delete invitation

---
+++ src/Application/Invitations/Commands/DeleteInvitation/DeleteInvitation.cs
@@ -0,0 +1,29 @@
+﻿using SharedCookbook.Application.Common.Interfaces;
+using SharedCookbook.Domain.Events;
+
+namespace SharedCookbook.Application.Invitations.Commands.DeleteInvitation;
+
+public record DeleteInvitationCommand(int Id) : IRequest;
+public class DeleteInvitationCommandHandler : IRequestHandler<DeleteInvitationCommand>
+{
+    private readonly IApplicationDbContext _context;
+
+    public DeleteInvitationCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Handle(DeleteInvitationCommand request, CancellationToken cancellationToken)
+    {
+        var entity = await _context.CookbookInvitations
+            .FindAsync([request.Id], cancellationToken);
+
+        Guard.Against.NotFound(request.Id, entity);
+
+        _context.CookbookInvitations.Remove(entity);
+
+        entity.AddDomainEvent(new InvitationDeletedEvent(entity));
+
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
