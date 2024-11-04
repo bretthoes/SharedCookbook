5582a99a28a181f3e1755d6a02561155c876f718
Brett Hoes <bretthoes@gmail.com>
2024-11-03 21:28:06 -0500

    move dto to separate file, set status in query

--- src/Application/Invitations/Queries/GetInvitationsWithPagination/GetInvitationsWithPagination.cs
+++ src/Application/Invitations/Queries/GetInvitationsWithPagination/GetInvitationsWithPagination.cs
@@ -1,12 +1,13 @@
 ﻿using SharedCookbook.Application.Common.Interfaces;
 using SharedCookbook.Application.Common.Mappings;
 using SharedCookbook.Application.Common.Models;
-using SharedCookbook.Domain.Entities;
+using SharedCookbook.Domain.Enums;
 
 namespace SharedCookbook.Application.Invitations.Queries.GetInvitationsWithPagination;
 
 public record GetInvitationsWithPaginationQuery : IRequest<PaginatedList<InvitationDto>>
 {
+    public CookbookInvitationStatus Status { get; init; } = CookbookInvitationStatus.Sent;
     public int PageNumber { get; init; } = 1;
     public int PageSize { get; init; } = 10;
 }
@@ -22,11 +23,11 @@ public class GetInvitationsWithPaginationQueryHandler : IRequestHandler<GetInvit
         _user = user;
     }
 
-    public Task<PaginatedList<InvitationDto>> Handle(GetInvitationsWithPaginationQuery request, CancellationToken cancellationToken)
+    public Task<PaginatedList<InvitationDto>> Handle(GetInvitationsWithPaginationQuery query, CancellationToken cancellationToken)
     {
         return _context.CookbookInvitations
             .AsNoTracking()
-            .Where(invitation => invitation.RecipientPersonId == _user.Id)
+            .Where(invitation => invitation.RecipientPersonId == _user.Id && invitation.InvitationStatus == query.Status)
             .Select(invitation => new InvitationDto {
                 Id = invitation.Id,
                 Created = invitation.Created,
@@ -35,24 +36,8 @@ public class GetInvitationsWithPaginationQueryHandler : IRequestHandler<GetInvit
                 CookbookImage = invitation.Cookbook == null ? "" : invitation.Cookbook.Image,
             })
             .OrderByDescending(c => c.Created)
-            .PaginatedListAsync(request.PageNumber, request.PageSize, cancellationToken);
+            .PaginatedListAsync(query.PageNumber, query.PageSize, cancellationToken);
 
             
     }
-}
-
-// TODO move
-public class InvitationDto
-{
-    public int Id { get; set; }
-
-    public string? SenderName { get; set; } = string.Empty;
-
-    public required string CookbookTitle { get; set; }
-
-    public string? CookbookImage { get; set; }
-
-    public DateTimeOffset Created { get; set; }
-
-    public int? CreatedBy { get; set; }
 }
---
+++ src/Application/Invitations/Queries/GetInvitationsWithPagination/InvitationDto.cs
@@ -0,0 +1,18 @@
+﻿namespace SharedCookbook.Application.Invitations.Queries.GetInvitationsWithPagination;
+
+public record InvitationDto
+{
+    public int Id { get; init; }
+
+    public string? SenderName { get; init; } = string.Empty;
+
+    public string? SenderEmail { get; init; } = string.Empty;
+
+    public required string CookbookTitle { get; init; }
+
+    public string? CookbookImage { get; init; }
+
+    public DateTimeOffset Created { get; init; }
+
+    public int? CreatedBy { get; init; }
+}
