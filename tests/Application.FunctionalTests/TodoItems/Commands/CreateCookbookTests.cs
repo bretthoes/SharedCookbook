f8228bcc30aac9be2c2fa95f30613bdf64004fb1
Brett Hoes <bretthoes@gmail.com>
2025-01-15 17:58:48 -0500

    update tests

---
+++ tests/Application.FunctionalTests/TodoItems/Commands/CreateCookbookTests.cs
@@ -0,0 +1,44 @@
+﻿using SharedCookbook.Application.Common.Exceptions;
+using SharedCookbook.Application.Cookbooks.Commands.CreateCookbook;
+using SharedCookbook.Domain.Entities;
+
+namespace SharedCookbook.Application.FunctionalTests.TodoItems.Commands;
+
+using static Testing;
+
+public class CreateCookbookTests : BaseTestFixture
+{
+    [Test]
+    public async Task ShouldRequireMinimumFields()
+    {
+        var command = new CreateCookbookCommand
+        {
+            Title = null!
+        };
+
+        await FluentActions.Invoking(() =>
+            SendAsync(command)).Should().ThrowAsync<ValidationException>();
+    }
+
+    [Test]
+    public async Task ShouldCreateCookbook()
+    {
+        var userId = await RunAsDefaultUserAsync();
+
+        var command = new CreateCookbookCommand
+        {
+            Title = "Another New Cookbook",
+        };
+
+        var itemId = await SendAsync(command);
+
+        var item = await FindAsync<Cookbook>(itemId);
+
+        item.Should().NotBeNull();
+        item!.Title.Should().Be(command.Title);
+        item.CreatedBy.Should().Be(userId);
+        item.Created.Should().BeCloseTo(DateTime.Now, TimeSpan.FromSeconds(10));
+        item.LastModifiedBy.Should().Be(userId);
+        item.LastModified.Should().BeCloseTo(DateTime.Now, TimeSpan.FromSeconds(10));
+    }
+}
