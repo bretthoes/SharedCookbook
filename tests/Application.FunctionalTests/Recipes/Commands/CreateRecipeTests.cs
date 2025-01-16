11abfbae5f830377ea15b537dc66998ea2fb4e7e
Brett Hoes <bretthoes@gmail.com>
2025-01-16 17:36:40 -0500

    add unit tests

---
+++ tests/Application.FunctionalTests/Recipes/Commands/CreateRecipeTests.cs
@@ -0,0 +1,53 @@
+﻿using SharedCookbook.Application.Common.Exceptions;
+using SharedCookbook.Application.Cookbooks.Commands.CreateCookbook;
+using SharedCookbook.Application.Recipes.Commands.CreateRecipe;
+using SharedCookbook.Domain.Entities;
+
+namespace SharedCookbook.Application.FunctionalTests.Recipes.Commands;
+
+using static Testing;
+
+public class CreateRecipeTests : BaseTestFixture
+{
+    [Test]
+    public async Task ShouldRequireMinimumFields()
+    {
+        var command = new CreateRecipeCommand
+        {
+            Recipe = new CreateRecipeDto
+            {
+                Title = null!,
+                CookbookId = 0
+            }
+        };
+
+        await FluentActions.Invoking(() =>
+            SendAsync(command)).Should().ThrowAsync<ValidationException>();
+    }
+
+    [Test]
+    public async Task ShouldCreateRecipe()
+    {
+        var userId = await RunAsDefaultUserAsync();
+
+        var command = new CreateRecipeCommand
+        {
+            Recipe = new CreateRecipeDto
+            {
+                Title = "Recipe Title",
+                CookbookId = 0
+            }
+        };
+
+        var itemId = await SendAsync(command);
+
+        var item = await FindAsync<Recipe>(itemId);
+
+        item.Should().NotBeNull();
+        item!.Title.Should().Be(command.Recipe.Title);
+        item.CreatedBy.Should().Be(userId);
+        item.Created.Should().BeCloseTo(DateTime.Now, TimeSpan.FromSeconds(10));
+        item.LastModifiedBy.Should().Be(userId);
+        item.LastModified.Should().BeCloseTo(DateTime.Now, TimeSpan.FromSeconds(10));
+    }
+}
---
+++ tests/Application.FunctionalTests/Recipes/Commands/DeleteRecipeTests.cs
@@ -0,0 +1,38 @@
+﻿using SharedCookbook.Application.Recipes.Commands.CreateRecipe;
+using SharedCookbook.Application.Recipes.Commands.DeleteRecipe;
+using SharedCookbook.Domain.Entities;
+
+namespace SharedCookbook.Application.FunctionalTests.Recipes.Commands;
+
+using static Testing;
+
+public class DeleteRecipeTests : BaseTestFixture
+{
+    [Test]
+    public async Task ShouldRequireValidRecipeId()
+    {
+        var command = new DeleteRecipeCommand(Id: 99);
+
+        await FluentActions.Invoking(() =>
+            SendAsync(command)).Should().ThrowAsync<NotFoundException>();
+    }
+
+    [Test]
+    public async Task ShouldDeleteRecipe()
+    {
+        var itemId = await SendAsync(new CreateRecipeCommand
+        {
+            Recipe = new CreateRecipeDto
+            {
+                Title = "Another Recipe Title",
+                CookbookId = 0
+            }
+        });
+
+        await SendAsync(new DeleteRecipeCommand(itemId));
+
+        var item = await FindAsync<Recipe>(itemId);
+
+        item.Should().BeNull();
+    }
+}
