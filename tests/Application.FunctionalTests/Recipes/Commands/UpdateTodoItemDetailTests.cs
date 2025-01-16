ed43ab2cc0eacb14433afec178a856439dae09d7
Brett Hoes <bretthoes@gmail.com>
2025-01-16 17:34:46 -0500

    update tests

---
+++ tests/Application.FunctionalTests/Recipes/Commands/UpdateTodoItemDetailTests.cs
@@ -0,0 +1,53 @@
+﻿using SharedCookbook.Application.Cookbooks.Commands.CreateCookbook;
+using SharedCookbook.Application.Recipes.Commands.CreateRecipe;
+using SharedCookbook.Application.Recipes.Commands.UpdateRecipe;
+using SharedCookbook.Domain.Entities;
+
+namespace SharedCookbook.Application.FunctionalTests.Recipes.Commands;
+
+using static Testing;
+
+public class UpdateRecipeTests : BaseTestFixture
+{
+    [Test]
+    public async Task ShouldRequireValidRecipeId()
+    {
+        var command = new UpdateRecipeCommand { Id = 99 };
+        await FluentActions.Invoking(() => SendAsync(command)).Should().ThrowAsync<NotFoundException>();
+    }
+
+    [Test]
+    public async Task ShouldUpdateRecipe() {
+        var userId = await RunAsDefaultUserAsync();
+
+        var cookbookId = await SendAsync(new CreateCookbookCommand
+        {
+            Title = "New Cookbook"
+        });
+
+        var recipeId = await SendAsync(new CreateRecipeCommand
+        {
+            Recipe = new CreateRecipeDto
+            {
+                Title = "New Recipe",
+                CookbookId = cookbookId,
+            }
+        });
+
+        var command = new UpdateRecipeCommand
+        {
+            Id = recipeId,
+            Title = "New Recipe Title",
+        };
+
+        await SendAsync(command);
+
+        var item = await FindAsync<Recipe>(recipeId);
+
+        item.Should().NotBeNull();
+        item!.Title.Should().Be(command.Title);
+        item.LastModifiedBy.Should().NotBeNull();
+        item.LastModifiedBy.Should().Be(userId);
+        item.LastModified.Should().BeCloseTo(DateTime.Now, TimeSpan.FromSeconds(10));
+    }
+}
--- tests/Application.UnitTests/Common/Behaviours/RequestLoggerTests.cs
+++ tests/Application.UnitTests/Common/Behaviours/RequestLoggerTests.cs
@@ -1,22 +1,22 @@
 ﻿using SharedCookbook.Application.Common.Behaviours;
 using SharedCookbook.Application.Common.Interfaces;
-using SharedCookbook.Application.TodoItems.Commands.CreateTodoItem;
 using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
+using SharedCookbook.Application.Cookbooks.Commands.CreateCookbook;
 
 namespace SharedCookbook.Application.UnitTests.Common.Behaviours;
 
 public class RequestLoggerTests
 {
-    private Mock<ILogger<CreateTodoItemCommand>> _logger = null!;
+    private Mock<ILogger<CreateCookbookCommand>> _logger = null!;
     private Mock<IUser> _user = null!;
     private Mock<IIdentityService> _identityService = null!;
 
     [SetUp]
     public void Setup()
     {
-        _logger = new Mock<ILogger<CreateTodoItemCommand>>();
+        _logger = new Mock<ILogger<CreateCookbookCommand>>();
         _user = new Mock<IUser>();
         _identityService = new Mock<IIdentityService>();
     }
@@ -27,9 +27,9 @@ public class RequestLoggerTests
         var id = 1;
         _user.Setup(x => x.Id).Returns(id);
 
-        var requestLogger = new LoggingBehaviour<CreateTodoItemCommand>(_logger.Object, _user.Object, _identityService.Object);
+        var requestLogger = new LoggingBehaviour<CreateCookbookCommand>(_logger.Object, _user.Object, _identityService.Object);
 
-        await requestLogger.Process(new CreateTodoItemCommand { ListId = 1, Title = "title" }, new CancellationToken());
+        await requestLogger.Process(new CreateCookbookCommand { Title = "title" }, new CancellationToken());
 
         _identityService.Verify(i => i.GetUserNameAsync(It.IsAny<int>()), Times.Once);
     }
@@ -37,9 +37,9 @@ public class RequestLoggerTests
     [Test]
     public async Task ShouldNotCallGetUserNameAsyncOnceIfUnauthenticated()
     {
-        var requestLogger = new LoggingBehaviour<CreateTodoItemCommand>(_logger.Object, _user.Object, _identityService.Object);
+        var requestLogger = new LoggingBehaviour<CreateCookbookCommand>(_logger.Object, _user.Object, _identityService.Object);
 
-        await requestLogger.Process(new CreateTodoItemCommand { ListId = 1, Title = "title" }, new CancellationToken());
+        await requestLogger.Process(new CreateCookbookCommand { Title = "title" }, new CancellationToken());
 
         _identityService.Verify(i => i.GetUserNameAsync(It.IsAny<int>()), Times.Never);
     }
