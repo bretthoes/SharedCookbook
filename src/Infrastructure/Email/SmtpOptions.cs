dec45f3492ae6d38b1cfb999975ac3961cf66b93
Brett Hoes <bretthoes@gmail.com>
2025-01-25 03:39:56 -0500

    add from params to smtp options

--- src/Infrastructure/Email/SmtpOptions.cs
+++ src/Infrastructure/Email/SmtpOptions.cs
@@ -2,6 +2,8 @@
 
 public class SmtpOptions
 {
+    public required string FromAddress { get; init; }
+    public required string FromAddressName { get; init; }
     public required string Host { get; init; }
     public required int Port { get; init; }
 }
