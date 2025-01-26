eb113ef9cddae86fdd92bc827608ebabfa4b62d0
Brett Hoes <bretthoes@gmail.com>
2025-01-26 15:52:51 -0500

    tiny refactor

--- src/Infrastructure/Email/EmailSender.cs
+++ src/Infrastructure/Email/EmailSender.cs
@@ -16,19 +16,27 @@ public class EmailSender(IOptions<EmailApiOptions> options) : IEmailSender
 
     public async Task SendEmailAsync(string email, string subject, string htmlMessage)
     {
+        var authenticator = new HttpBasicAuthenticator("api", _options.ApiKey); 
+        
         var options = new RestClientOptions(_options.BaseUrl) {
-            Authenticator = new HttpBasicAuthenticator("api", _options.ApiKey)
+            Authenticator = authenticator
         };
+        
         var client = new RestClient(options);
         
-        var request = new RestRequest();
+        var request = new RestRequest
+        {
+            Resource = "{domain}/messages",
+            Method = Method.Post
+        };
+        
+        // add email parameters to request
         request.AddParameter ("domain", _options.Domain, ParameterType.UrlSegment);
-        request.Resource = "{domain}/messages";
         request.AddParameter ("from", _options.From);
         request.AddParameter ("to", email);
         request.AddParameter ("subject", subject);
         request.AddParameter ("html", htmlMessage);
-        request.Method = Method.Post;
-        var result = await client.ExecuteAsync(request);
+        
+        await client.ExecuteAsync(request);
     }
 }
