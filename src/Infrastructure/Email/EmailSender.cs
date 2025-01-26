c58d080c367a03c2d69e2e8611bd47041a4339ae
Brett Hoes <bretthoes@gmail.com>
2025-01-26 18:51:22 -0500

    remove usings

--- src/Infrastructure/Email/EmailSender.cs
+++ src/Infrastructure/Email/EmailSender.cs
@@ -1,12 +1,7 @@
-﻿using MailKit.Net.Smtp;
-using Microsoft.AspNetCore.Identity.UI.Services;
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.Extensions.Options;
-using MimeKit;
-using MimeKit.Text;
-using Org.BouncyCastle.Crypto.Digests;
 using RestSharp;
 using RestSharp.Authenticators;
-using Parameter = RestSharp.Parameter;
 
 namespace SharedCookbook.Infrastructure.Email;
 
@@ -16,27 +11,28 @@ public class EmailSender(IOptions<EmailApiOptions> options) : IEmailSender
 
     public async Task SendEmailAsync(string email, string subject, string htmlMessage)
     {
-        var authenticator = new HttpBasicAuthenticator("api", _options.ApiKey); 
-        
-        var options = new RestClientOptions(_options.BaseUrl) {
+        var authenticator = new HttpBasicAuthenticator("api", _options.ApiKey);
+
+        var options = new RestClientOptions(_options.BaseUrl)
+        {
             Authenticator = authenticator
         };
-        
+
         var client = new RestClient(options);
-        
+
         var request = new RestRequest
         {
             Resource = "{domain}/messages",
             Method = Method.Post
         };
-        
+
         // add email parameters to request
-        request.AddParameter ("domain", _options.Domain, ParameterType.UrlSegment);
-        request.AddParameter ("from", _options.From);
-        request.AddParameter ("to", email);
-        request.AddParameter ("subject", subject);
-        request.AddParameter ("html", htmlMessage);
-        
+        request.AddParameter("domain", _options.Domain, ParameterType.UrlSegment);
+        request.AddParameter("from", _options.From);
+        request.AddParameter("to", email);
+        request.AddParameter("subject", subject);
+        request.AddParameter("html", htmlMessage);
+
         await client.ExecuteAsync(request);
     }
 }
