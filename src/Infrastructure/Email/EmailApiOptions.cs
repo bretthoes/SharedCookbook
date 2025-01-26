963a525738dbcddea22829812154457374354b68
Brett Hoes <bretthoes@gmail.com>
2025-01-26 15:44:25 -0500

    add email api options

---
+++ src/Infrastructure/Email/EmailApiOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace SharedCookbook.Infrastructure.Email;
+
+public class EmailApiOptions
+{
+    public required string ApiKey { get; init; }
+    public required string BaseUrl { get; init; }
+    public required string Domain { get; init; }
+    public required string From { get; init; }
+}
