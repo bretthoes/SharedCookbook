9a3f980fd0456807fb5e7b3f61423cdd1d6dfab5
Brett Hoes <bretthoes@gmail.com>
2025-02-28 18:26:08 -0500

    add ocr service

---
+++ src/Application/Common/Interfaces/IOcrService.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace SharedCookbook.Application.Common.Interfaces;
+
+public interface IOcrService
+{
+    Task<string> ExtractText(IFormFile file);
+}
---
+++ src/Infrastructure/OpticalCharacterRecognition/TesseractOcrService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using SharedCookbook.Application.Common.Interfaces;
+using Tesseract;
+
+namespace SharedCookbook.Infrastructure.OpticalCharacterRecognition;
+
+public class TesseractOcrService : IOcrService
+{
+    private const string DataPath = "./tessdata";
+    private const string Language = "eng";
+    public async Task<string> ExtractText(IFormFile file)
+    {
+        await using var stream = file.OpenReadStream();
+        using var engine = new TesseractEngine(@DataPath, Language, EngineMode.Default);
+        using var img = Pix.LoadFromMemory(await ReadStreamAsync(stream));
+        using var page = engine.Process(img);
+
+        return page.GetText();
+    }
+
+    private static async Task<byte[]> ReadStreamAsync(Stream stream)
+    {
+        using var memoryStream = new MemoryStream();
+        await stream.CopyToAsync(memoryStream);
+        return memoryStream.ToArray();
+    }
+}
