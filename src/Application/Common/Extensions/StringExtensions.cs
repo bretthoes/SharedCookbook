cc4fd768c6fd6b33b983265307e4b80e9dad67eb
brett <bretthoes@gmail.com>
2025-03-09 10:05:39 -0400

    refactor

--- src/Application/Common/Extensions/StringExtensions.cs
+++ src/Application/Common/Extensions/StringExtensions.cs
@@ -3,12 +3,8 @@ namespace SharedCookbook.Application.Common.Extensions
     public static class StringExtensions
     {
         public static bool IsValidUrl(this string url)
-        {
-            if (Uri.TryCreate(url, UriKind.Absolute, out Uri? uriResult))
-                return uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps;
-            
-            return false;
-        }
+            => (Uri.TryCreate(url, UriKind.Absolute, out Uri? uriResult)) 
+               && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
 
         public static bool HasValidImageExtension(this string fileName)
             => ImageUtilities.AllowedExtensions
