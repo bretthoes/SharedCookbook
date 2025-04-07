58c876c9af5aac71fef99ee674c997d0c2813c53
Brett Hoes <bretthoes@gmail.com>
2025-04-07 08:33:52 -0400

    specify fromBody in update cookbook endpoint

--- src/Web/Endpoints/Cookbooks.cs
+++ src/Web/Endpoints/Cookbooks.cs
@@ -1,4 +1,5 @@
-﻿using SharedCookbook.Application.Common.Models;
+﻿using Microsoft.AspNetCore.Mvc;
+using SharedCookbook.Application.Common.Models;
 using SharedCookbook.Application.Cookbooks.Commands.CreateCookbook;
 using SharedCookbook.Application.Cookbooks.Commands.DeleteCookbook;
 using SharedCookbook.Application.Cookbooks.Commands.UpdateCookbook;
@@ -14,28 +15,33 @@ public class Cookbooks : EndpointGroupBase
             .RequireAuthorization()
             .MapGet(GetCookbooksWithPagination)
             .MapPost(CreateCookbook)
-            .MapPut(UpdateCookbook, "{id}")
-            .MapDelete(DeleteCookbook, "{id}");
+            .MapPut(UpdateCookbook, pattern: "{id}")
+            .MapDelete(DeleteCookbook, pattern: "{id}");
     }
 
-    public Task<PaginatedList<CookbookBriefDto>> GetCookbooksWithPagination(ISender sender, [AsParameters] GetCookbooksWithPaginationQuery query)
+    private static Task<PaginatedList<CookbookBriefDto>> GetCookbooksWithPagination(
+        ISender sender,
+        [AsParameters] GetCookbooksWithPaginationQuery query)
     {
         return sender.Send(query);
     }
 
-    public Task<int> CreateCookbook(ISender sender, CreateCookbookCommand command)
+    private static Task<int> CreateCookbook(ISender sender, CreateCookbookCommand command)
     {
         return sender.Send(command);
     }
 
-    public async Task<IResult> UpdateCookbook(ISender sender, int id, UpdateCookbookCommand command)
+    private static async Task<IResult> UpdateCookbook(
+        ISender sender,
+        int id,
+        [FromBody] UpdateCookbookCommand command)
     {
         if (id != command.Id) return Results.BadRequest();
         await sender.Send(command);
         return Results.NoContent();
     }
 
-    public async Task<IResult> DeleteCookbook(ISender sender, int id)
+    private static async Task<IResult> DeleteCookbook(ISender sender, int id)
     {
         await sender.Send(new DeleteCookbookCommand(id));
         return Results.NoContent();
