aa8a987ae6f72ceca1c98105114925b8f31f363b
brett <bretthoes@gmail.com>
2025-04-18 13:39:01 -0400

    update accepted extension list for imgs

--- src/Application/Common/ImageUtilities.cs
+++ src/Application/Common/ImageUtilities.cs
@@ -6,7 +6,9 @@ public static class ImageUtilities
     public const long MaxFileSizeBytes = MaxFileSizeMegabytes * 1024 * 1024;
     public const short MaxFileUploadCount = 6;
     // TODO update this with broader list of valid image extensions, test each new one
-    public static readonly string[] AllowedExtensions = [".jpg", ".png", ".jpeg"];
+    public static readonly string[] AllowedExtensions = [
+        ".jpg", ".jpeg", ".png", ".gif", ".bmp", ".webp", ".heic", ".heif", ".tiff"
+    ];
 
     public static string GetUniqueFileName(string extension) => $"{Guid.NewGuid()}{extension}";
 }
