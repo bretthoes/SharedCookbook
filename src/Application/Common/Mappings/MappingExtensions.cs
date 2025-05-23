78dcf39d2b4e67fcdc51357e9ee6c48a8e520540
Brett Hoes <bretthoes@gmail.com>
2025-05-23 18:14:10 -0400

    update more names

--- src/Application/Common/Mappings/MappingExtensions.cs
+++ src/Application/Common/Mappings/MappingExtensions.cs
@@ -6,5 +6,5 @@ public static class MappingExtensions
 {
     public static Task<PaginatedList<TDestination>> PaginatedListAsync<TDestination>(
         this IQueryable<TDestination> queryable, int pageNumber, int pageSize, CancellationToken cancellationToken) where TDestination : class
-        => PaginatedList<TDestination>.CreateAsync(queryable.AsNoTracking(), pageNumber, pageSize);
+        => PaginatedList<TDestination>.CreateAsync(source: queryable.AsNoTracking(), pageNumber, pageSize);
 }
