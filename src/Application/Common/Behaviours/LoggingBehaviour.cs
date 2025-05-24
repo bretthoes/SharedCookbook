c98f5b56702c3b5cb74d0147f6b564399dca882e
Brett Hoes <bretthoes@gmail.com>
2025-05-24 09:56:53 -0400

    rename

--- src/Application/Common/Behaviours/LoggingBehaviour.cs
+++ src/Application/Common/Behaviours/LoggingBehaviour.cs
@@ -18,7 +18,7 @@ public class LoggingBehaviour<TRequest> : IRequestPreProcessor<TRequest> where T
     public async Task Process(TRequest request, CancellationToken cancellationToken)
     {
         var requestName = typeof(TRequest).Name;
-        var userId = _user.Id ?? string.Empty;
+        string userId = _user.Id ?? string.Empty;
         string? userName = string.Empty;
 
         if (!string.IsNullOrEmpty(userId))
