46f4adab87062732645ebf7452fb90f6c2e33937
Brett Hoes <bretthoes@gmail.com>
2025-08-01 22:51:26 -0400

    linting; primary ctor

--- src/Application/Invitations/Commands/CreateInvitation/CreateInvitation.cs
+++ src/Application/Invitations/Commands/CreateInvitation/CreateInvitation.cs
@@ -11,19 +11,9 @@ public record CreateInvitationCommand : IRequest<int>
     public required string Email { get; init; }
 }
 
-public class CreateInvitationCommandHandler : IRequestHandler<CreateInvitationCommand, int>
+public class CreateInvitationCommandHandler(IApplicationDbContext context, IIdentityService identityService, IUser user)
+    : IRequestHandler<CreateInvitationCommand, int>
 {
-    private readonly IApplicationDbContext _context;
-    private readonly IIdentityService _identityService;
-    private readonly IUser _user;
-
-    public CreateInvitationCommandHandler(IApplicationDbContext context, IIdentityService identityService, IUser user)
-    {
-        _context = context;
-        _identityService = identityService;
-        _user = user;
-    }
-
     public async Task<int> Handle(CreateInvitationCommand command, CancellationToken cancellationToken)
     {
         var recipientId = await GetRecipientId(command.Email);
@@ -35,20 +25,20 @@ public class CreateInvitationCommandHandler : IRequestHandler<CreateInvitationCo
 
     private async Task<string> GetRecipientId(string email)
     {
-        var recipient = await _identityService.FindByEmailAsync(email);
+        var recipient = await identityService.FindByEmailAsync(email);
 
         return recipient?.Id ?? throw new NotFoundException(email, nameof(UserDto));
     }
 
     private async Task ValidateInvitation(int cookbookId, string recipientId, CancellationToken token)
     {
-        var recipientIsAlreadyMember = await _context.CookbookMemberships
+        var recipientIsAlreadyMember = await context.CookbookMemberships
             .AnyAsync(member => member.CreatedBy == recipientId
                 && member.CookbookId == cookbookId,
                 token);
         if (recipientIsAlreadyMember) throw new ConflictException("Recipient is already a member of this cookbook.");
 
-        var recipientHasPendingInvite = await _context.CookbookInvitations
+        var recipientHasPendingInvite = await context.CookbookInvitations
             .AnyAsync(invitation => invitation.RecipientPersonId == recipientId
                 && invitation.CookbookId == cookbookId
                 && invitation.InvitationStatus == CookbookInvitationStatus.Sent,
@@ -61,16 +51,16 @@ public class CreateInvitationCommandHandler : IRequestHandler<CreateInvitationCo
         var entity = new CookbookInvitation
         {
             CookbookId = cookbookId,
-            CreatedBy = _user.Id,
+            CreatedBy = user.Id,
             RecipientPersonId = recipientId,
             InvitationStatus = CookbookInvitationStatus.Sent,
             ResponseDate = null,
         };
 
-        _context.CookbookInvitations.Add(entity);
+        context.CookbookInvitations.Add(entity);
         entity.AddDomainEvent(new InvitationCreatedEvent(entity));
 
-        await _context.SaveChangesAsync(token);
+        await context.SaveChangesAsync(token);
 
         return entity.Id;
     }
