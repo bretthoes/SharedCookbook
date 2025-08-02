8512aaf610533522a2502b012da3e73ac9bf7f53
Brett Hoes <bretthoes@gmail.com>
2025-08-02 11:46:36 -0400

    add command for creating membership

---
+++ src/Application/Memberships/Commands/CreateMembership/CreateMembership.cs
@@ -0,0 +1,31 @@
+﻿namespace SharedCookbook.Application.Memberships.Commands.CreateMembership;
+
+public record CreateMembershipCommand : IRequest
+{
+    public int CookbookId { get; init; }
+}
+
+public class CreateMembershipCommandHandler(IApplicationDbContext context, IUser user)
+    : IRequestHandler<CreateMembershipCommand>
+{
+    public async Task Handle(CreateMembershipCommand command, CancellationToken token)
+    {
+        Guard.Against.Null(user.Id);
+        
+        if (await MembershipAlreadyExistsInCookbook(command.CookbookId, user.Id, token)) return;
+        
+        var membership = CookbookMembership.GetDefaultMembership(command.CookbookId);
+
+        await context.CookbookMemberships.AddAsync(membership, token);
+        await context.SaveChangesAsync(token);
+    }
+    
+    private async Task<bool> MembershipAlreadyExistsInCookbook(
+            int cookbookId,
+            string userId,
+            CancellationToken token) 
+            => await context.CookbookMemberships
+                .AnyAsync(membership
+                    => membership.CookbookId == cookbookId
+                       && membership.CreatedBy == userId, token);
+}
