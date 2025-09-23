f16415617bb073cfbbd1eade13430be20e9c45fc
Brett Hoes <bretthoes@gmail.com>
2025-09-23 15:12:18 -0400

    add domain exceptions for new val obj

---
+++ src/Domain/Exceptions/InvalidHashLengthException.cs
@@ -0,0 +1,6 @@
+﻿using SharedCookbook.Domain.ValueObjects;
+
+namespace SharedCookbook.Domain.Exceptions;
+
+public class InvalidHashLengthException(int length) 
+    : Exception($"Invalid hash length provided: \"{length}\". Expected was: \"{TokenDigest.HashLength}\".");
---
+++ src/Domain/Exceptions/InvalidSaltLengthException.cs
@@ -0,0 +1,6 @@
+﻿using SharedCookbook.Domain.ValueObjects;
+
+namespace SharedCookbook.Domain.Exceptions;
+
+public class InvalidSaltLengthException(int length) 
+    : Exception($"Invalid salt length provided: \"{length}\". Expected was: \"{TokenDigest.SaltLength}\".");
