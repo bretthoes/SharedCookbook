894ff967ac015314e4552388f0d9f458b9f6e4ad
Brett Hoes <bretthoes@gmail.com>
2025-10-01 12:32:51 -0400

    move rate limiter to its own file

---
+++ src/Web/Infrastructure/RateLimiterExtension.cs
@@ -0,0 +1,52 @@
+﻿using System.Globalization;
+using System.Threading.RateLimiting;
+
+namespace SharedCookbook.Web.Infrastructure;
+
+public static class RateLimiterExtension
+{
+    internal static void AddRateLimiter(this IHostApplicationBuilder builder)
+    {
+        builder.Services.AddRateLimiter(options =>
+        {
+            options.OnRejected = async (context, cancellationToken) =>
+            {
+                if (context.Lease.TryGetMetadata(MetadataName.RetryAfter, out var retryAfter))
+                {
+                    context.HttpContext.Response.Headers.RetryAfter =
+                        ((int) retryAfter.TotalSeconds).ToString(NumberFormatInfo.InvariantInfo);
+                }
+                
+                context.HttpContext.Response.StatusCode = StatusCodes.Status429TooManyRequests;
+                await context.HttpContext.Response.WriteAsync("Too many requests. Please try again later.", cancellationToken);
+            };
+            options.GlobalLimiter = PartitionedRateLimiter.CreateChained(
+                PartitionedRateLimiter.Create<HttpContext, string>(partitioner: httpContext =>
+                {
+                    var userAgent = httpContext.Request.Headers.UserAgent.ToString();
+
+                    return RateLimitPartition.GetFixedWindowLimiter
+                    (userAgent, factory: _ =>
+                        new FixedWindowRateLimiterOptions
+                        {
+                            AutoReplenishment = true,
+                            PermitLimit = 4,
+                            Window = TimeSpan.FromSeconds(2)
+                        });
+                }),
+                PartitionedRateLimiter.Create<HttpContext, string>(partitioner: httpContext =>
+                {
+                    var userAgent = httpContext.Request.Headers.UserAgent.ToString();
+            
+                    return RateLimitPartition.GetFixedWindowLimiter
+                    (userAgent, factory: _ =>
+                        new FixedWindowRateLimiterOptions
+                        {
+                            AutoReplenishment = true,
+                            PermitLimit = 15,
+                            Window = TimeSpan.FromSeconds(30)
+                        });
+                }));
+        });
+    }
+}
