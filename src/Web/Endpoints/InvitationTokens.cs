3e6a30ed9cef246c511a079436243921ec486a64
Brett Hoes <bretthoes@gmail.com>
2025-11-05 17:52:27 -0500

    add getter endpoint for inv token

--- src/Web/Endpoints/InvitationTokens.cs
+++ src/Web/Endpoints/InvitationTokens.cs
@@ -1,4 +1,6 @@
-﻿using SharedCookbook.Application.InvitationTokens.Commands.CreateInvitationToken;
+﻿using SharedCookbook.Application.Invitations.Queries.GetInvitationsWithPagination;
+using SharedCookbook.Application.InvitationTokens.Commands.CreateInvitationToken;
+using SharedCookbook.Application.InvitationTokens.Queries.GetInvitationToken;
 
 namespace SharedCookbook.Web.Endpoints;
 
@@ -6,8 +8,12 @@ public class InvitationTokens : EndpointGroupBase
 {
     public override void Map(RouteGroupBuilder builder)
     {
+        builder.MapGet(GetInvitationToken).RequireAuthorization();
         builder.MapPost(CreateInvitationToken).RequireAuthorization();
     }
+    
+    private static Task<InvitationDto> GetInvitationToken(ISender sender, GetInvitationTokenQuery query)
+        => sender.Send(query);
     
     private static Task<string> CreateInvitationToken(ISender sender, CreateInvitationTokenCommand command)
         => sender.Send(command);
