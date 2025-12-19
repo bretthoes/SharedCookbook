7850bff80897dc72cdf6bce58413eb9bced9dd72
Brett Hoes <bretthoes@gmail.com>
2025-12-18 23:03:05 -0500

    remove ext

--- src/Application/Contracts/RecipeDirectionDto.cs
+++ src/Application/Contracts/RecipeDirectionDto.cs
@@ -17,8 +17,4 @@ public static partial class Extensions
     public static IEnumerable<RecipeDirection> ToEntities(
         this IEnumerable<RecipeDirectionDto> recipeIngredients)
         => recipeIngredients.Select(direction => direction.ToEntity());
-
-    // TODO change extension to expression
-    public static RecipeDirectionDto ToDto(this RecipeDirection recipeDirection)
-        => new() { Text = recipeDirection.Text, Ordinal = recipeDirection.Ordinal, Image = recipeDirection.Image };
 }
