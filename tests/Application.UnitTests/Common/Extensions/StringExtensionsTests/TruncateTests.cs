377b4ccc0392369a862ef8eaf0a6b258776b4c37
thinkpad <bretthoes@gmail.com>
2025-12-22 23:10:40 -0500

    truncate tests

---
+++ tests/Application.UnitTests/Common/Extensions/StringExtensionsTests/TruncateTests.cs
@@ -0,0 +1,27 @@
+using SharedCookbook.Application.Common.Extensions;
+
+namespace SharedCookbook.Application.UnitTests.Common.Extensions;
+
+public class TruncateTests
+{
+    [TestCase("", 0, "")]
+    [TestCase("", 5, "")]
+    [TestCase("a", 0, "")]
+    [TestCase("a", 1, "a")]
+    [TestCase("abc", 2, "ab")]
+    [TestCase("abc", 3, "abc")]
+    [TestCase("abc", 10, "abc")]
+    [TestCase("hello world", 5, "hello")]
+    public void ReturnsExpected(string input, int maxLength, string expected)
+    {
+        string actual = input.Truncate(maxLength);
+
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void ThrowsWhenMaxLengthIsNegative()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => _ = string.Empty.Truncate(-1));
+    }
+}
