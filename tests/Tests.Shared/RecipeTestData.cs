dda03abedc72dfd4c0e925320ac1b0fd320dc01f
brett <bretthoes@gmail.com>
2026-01-18 23:00:06 -0500

    add common recipe data

---
+++ tests/Tests.Shared/RecipeTestData.cs
@@ -0,0 +1,21 @@
+namespace SharedCookbook.Tests.Shared;
+
+public static class RecipeTestData
+{
+    public const string Title = "Delicious Pasta";
+    public const string Summary = "A wonderful pasta dish";
+    public const int Servings = 4;
+    public const int PreparationTimeInMinutes = 10;
+    public const int CookingTimeInMinutes = 20;
+    public const int BakingTimeInMinutes = 5;
+    
+    public const string IngredientName = "Spaghetti";
+    public const int IngredientOrdinal = 1;
+    public const bool IngredientOptional = false;
+    
+    public const string DirectionText = "Boil water and cook pasta";
+    public const int DirectionOrdinal = 1;
+    
+    public const string ImageName = "pasta-image.jpg";
+    public const int ImageOrdinal = 1;
+}
