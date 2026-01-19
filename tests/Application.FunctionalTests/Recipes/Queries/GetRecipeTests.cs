e0a05dc28ed6c4b268270f82af0b9cb8121fed11
brett <bretthoes@gmail.com>
2026-01-18 23:13:25 -0500

    tests for get recipe; prefixed image url

--- tests/Application.FunctionalTests/Recipes/Queries/GetRecipeTests.cs
+++ tests/Application.FunctionalTests/Recipes/Queries/GetRecipeTests.cs
@@ -75,21 +75,21 @@ public class GetRecipeTests : BaseTestFixture
     public void ShouldHaveId() => Assert.That(_actual!.Id, Is.EqualTo(_recipeId));
     
     [Test]
-    public void ShouldHaveTitle() => Assert.That(_actual!.Title, Is.EqualTo(Title));
+    public void ShouldHaveTitle() => Assert.That(_actual!.Title, Is.EqualTo(expected: Title));
     
     [Test]
-    public void ShouldHaveSummary() => Assert.That(_actual!.Summary, Is.EqualTo(Summary));
+    public void ShouldHaveSummary() => Assert.That(_actual!.Summary, Is.EqualTo(expected: Summary));
     
     [Test]
-    public void ShouldHaveServings() => Assert.That(_actual!.Servings, Is.EqualTo(Servings));
+    public void ShouldHaveServings() => Assert.That(_actual!.Servings, Is.EqualTo(expected: Servings));
     
     [Test]
     public void ShouldHavePreparationTimeInMinutes()
-        => Assert.That(_actual!.PreparationTimeInMinutes, Is.EqualTo(PreparationTimeInMinutes));
+        => Assert.That(_actual!.PreparationTimeInMinutes, Is.EqualTo(expected: PreparationTimeInMinutes));
     
     [Test]
     public void ShouldHaveCookingTimeInMinutes()
-        => Assert.That(_actual!.CookingTimeInMinutes, Is.EqualTo(CookingTimeInMinutes));
+        => Assert.That(_actual!.CookingTimeInMinutes, Is.EqualTo(expected: CookingTimeInMinutes));
     
     [Test]
     public void ShouldHaveBakingTimeInMinutes()
@@ -97,41 +97,45 @@ public class GetRecipeTests : BaseTestFixture
 
     [Test]
     public void ShouldHaveIngredients()
-        => Assert.That(_actual!.Ingredients, Has.Count.EqualTo(1));
+        => Assert.That(_actual!.Ingredients, Has.Count.EqualTo(expected: 1));
     
     [Test]
     public void ShouldHaveIngredientName()
-        => Assert.That(_actual!.Ingredients.First().Name, Is.EqualTo(IngredientName));
+        => Assert.That(_actual!.Ingredients.First().Name, Is.EqualTo(expected: IngredientName));
     
     [Test]
     public void ShouldHaveIngredientOrdinal()
-        => Assert.That(_actual!.Ingredients.First().Ordinal, Is.EqualTo(IngredientOrdinal));
+        => Assert.That(_actual!.Ingredients.First().Ordinal, Is.EqualTo(expected: IngredientOrdinal));
     
     [Test]
     public void ShouldHaveIngredientOptional()
-        => Assert.That(_actual!.Ingredients.First().Optional, Is.EqualTo(IngredientOptional));
+        => Assert.That(_actual!.Ingredients.First().Optional, Is.EqualTo(expected: IngredientOptional));
 
     [Test]
     public void ShouldHaveDirections()
-        => Assert.That(_actual!.Directions, Has.Count.EqualTo(1));
+        => Assert.That(_actual!.Directions, Has.Count.EqualTo(expected: 1));
     
     [Test]
     public void ShouldHaveDirectionText()
-        => Assert.That(_actual!.Directions.First().Text, Is.EqualTo(DirectionText));
+        => Assert.That(_actual!.Directions.First().Text, Is.EqualTo(expected: DirectionText));
     
     [Test]
     public void ShouldHaveDirectionOrdinal()
-        => Assert.That(_actual!.Directions.First().Ordinal, Is.EqualTo(DirectionOrdinal));
+        => Assert.That(_actual!.Directions.First().Ordinal, Is.EqualTo(expected: DirectionOrdinal));
 
     [Test]
     public void ShouldHaveImages()
-        => Assert.That(_actual!.Images, Has.Count.EqualTo(1));
+        => Assert.That(_actual!.Images, Has.Count.EqualTo(expected: 1));
     
     [Test]
-    public void ShouldHaveImageName()
-        => Assert.That(_actual!.Images.First().Name, Is.EqualTo(ImageName));
+    public void ShouldHaveImageUrlPrefix()
+        => Assert.That(_actual!.Images.First().Name, Does.StartWith(expected: "http"));
+    
+    [Test]
+    public void ShouldHaveImageNameSuffix()
+        => Assert.That(_actual!.Images.First().Name, Does.EndWith(expected: ImageName));
     
     [Test]
     public void ShouldHaveImageOrdinal()
-        => Assert.That(_actual!.Images.First().Ordinal, Is.EqualTo(ImageOrdinal));
+        => Assert.That(_actual!.Images.First().Ordinal, Is.EqualTo(expected: ImageOrdinal));
 }
