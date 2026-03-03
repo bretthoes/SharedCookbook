ce437121b0c975f60d182e729361eac17f776cfc
brett <bretthoes@gmail.com>
2026-03-02 23:35:23 -0500

    move task to handler

--- src/Web/Endpoints/Users.cs
+++ src/Web/Endpoints/Users.cs
@@ -30,5 +30,5 @@ public class Users : EndpointGroupBase
         return Results.NoContent();
     }
 
-    private static async Task<DisplayNameDto> GetDisplayName(ISender sender) => await sender.Send(new GetDisplayNameQuery());
+    private static Task<DisplayNameDto> GetDisplayName(ISender sender) => sender.Send(new GetDisplayNameQuery());
 }
