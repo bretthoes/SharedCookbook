5abb276028ee4e3d6d7e11655c1e378dc2d5fd67
= <=>
2026-05-30 20:42:22 -0400

    add test

---
+++ tests/Application.FunctionalTests/Infrastructure/TestAuthHandler.cs
@@ -0,0 +1,33 @@
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace SharedCookbook.Application.FunctionalTests.Infrastructure;
+
+internal sealed class TestAuthHandler(
+    IOptionsMonitor<AuthenticationSchemeOptions> options,
+    ILoggerFactory logger,
+    UrlEncoder encoder)
+    : AuthenticationHandler<AuthenticationSchemeOptions>(options, logger, encoder)
+{
+    internal const string SchemeName = "Test";
+    internal const string UserIdHeaderName = "X-Test-User-Id";
+
+    protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        if (!Request.Headers.TryGetValue(UserIdHeaderName, out var userIdValues)
+            || string.IsNullOrWhiteSpace(userIdValues.FirstOrDefault()))
+        {
+            return Task.FromResult(AuthenticateResult.NoResult());
+        }
+
+        var claims = new[] { new Claim(ClaimTypes.NameIdentifier, userIdValues.First()!) };
+        var identity = new ClaimsIdentity(claims, authenticationType: SchemeName);
+        var principal = new ClaimsPrincipal(identity);
+        var ticket = new AuthenticationTicket(principal, SchemeName);
+
+        return Task.FromResult(AuthenticateResult.Success(ticket));
+    }
+}
---
+++ tests/Application.FunctionalTests/Memberships/Commands/WhenOwnerPromotesContributorToOwner.cs
@@ -0,0 +1,69 @@
+using SharedCookbook.Application.Memberships.Commands.UpdateMembership;
+using SharedCookbook.Domain.Entities;
+
+namespace SharedCookbook.Application.FunctionalTests.Memberships.Commands;
+
+using static Testing;
+
+public class WhenOwnerPromotesContributorToOwner : BaseTestFixture
+{
+    private const string ContributorEmail = "contributor@test.com";
+    private const string ContributorPassword = "Testing1234!";
+    private const string CookbookTitle = "Existing Cookbook";
+
+    private string _originalOwnerUserId = null!;
+    private string _contributorUserId = null!;
+    private CookbookMembership _demotedOwner = null!;
+    private CookbookMembership _promotedOwner = null!;
+
+    [OneTimeSetUp]
+    public async Task OneTimeSetup()
+    {
+        await ResetState();
+
+        _contributorUserId = await RunAsUserAsync(ContributorEmail, ContributorPassword, []);
+        var contributorMembership = new CookbookMembership { CreatedBy = _contributorUserId };
+
+        _originalOwnerUserId = await RunAsDefaultUserAsync();
+        var ownerMembership = CookbookMembership.NewOwner(_originalOwnerUserId);
+
+        await AddAsync(new Cookbook
+        {
+            Title = CookbookTitle,
+            CreatedBy = _originalOwnerUserId,
+            Memberships = [ownerMembership, contributorMembership],
+        });
+
+        await SendAsync(new UpdateMembershipCommand
+        {
+            Id = contributorMembership.Id,
+            IsOwner = true,
+            CanAddRecipe = false,
+            CanUpdateRecipe = false,
+            CanDeleteRecipe = false,
+            CanSendInvite = false,
+            CanRemoveMember = false,
+            CanEditCookbookDetails = false,
+        });
+
+        var memberships = await ListAsync<CookbookMembership>();
+        _demotedOwner = memberships.Single(m => m.CreatedBy == _originalOwnerUserId);
+        _promotedOwner = memberships.Single(m => m.CreatedBy == _contributorUserId);
+    }
+
+    [Test]
+    public void ShouldDemoteOriginalOwner() =>
+        Assert.That(_demotedOwner.IsOwner, Is.False);
+
+    [Test]
+    public void ShouldPromoteContributor() =>
+        Assert.That(_promotedOwner.IsOwner, Is.True);
+
+    [Test]
+    public void ShouldRecordDemotionModifiedBy() =>
+        Assert.That(_demotedOwner.LastModifiedBy, Is.EqualTo(_originalOwnerUserId));
+
+    [Test]
+    public void ShouldRecordPromotionModifiedBy() =>
+        Assert.That(_promotedOwner.LastModifiedBy, Is.EqualTo(_originalOwnerUserId));
+}
