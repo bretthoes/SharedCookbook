4d5da64028622045c91a4eef90fc6783072fdfd7
brett <bretthoes@gmail.com>
2026-06-13 22:34:39 -0400

    add todo

--- src/Application/Common/Extensions/DbSetExtensions.cs
+++ src/Application/Common/Extensions/DbSetExtensions.cs
@@ -2,6 +2,7 @@
 
 public static class DbSetExtensions
 {
+    // TODO revisit; it's very possible that FindAsync is not a good option for all usages of this; if the entity is not in memory, EF Core will fallback to SingleAsync (verify this). If it is the case that all usages would not have the object in memory (i.e. first line of a handler), replace this with EF Core's Single.
     public static async Task<TEntity> FindOrThrowAsync<TEntity>(
         this DbSet<TEntity> set,
         object id,
